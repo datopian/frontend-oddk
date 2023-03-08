34600e48f41f9a47a40ae7768862c497d8db67f9
Michael Polidori <contact@michaelpolidori.com>
2023-03-08 10:44:54 -0500

    Hardcode https for sitemap/robots.txt

--- index.js
+++ index.js
@@ -284,7 +284,7 @@ module.exports = function (app) {
       const robotsTxt = fs.readFileSync(robotsPath, 'utf8')
 
       if (!robotsTxt.includes('Sitemap:')) {
-        const hostname = config.get('SITE_URL')
+        const hostname = 'https://' + req.get('host')
         const sitemapUrl = hostname + '/sitemap.xml'
         const wwwHostname = hostname.replace('://', '://www.')
         const wwwSitemapUrl = wwwHostname + '/sitemap.xml'
@@ -304,7 +304,7 @@ module.exports = function (app) {
     res.header('Content-Type', 'application/xml');
     res.header('Content-Encoding', 'gzip');
 
-    const hostname = config.get('SITE_URL')
+    const hostname = 'https://' + req.get('host')
 
     try {
       const smStream = new SitemapStream({ hostname: hostname })
